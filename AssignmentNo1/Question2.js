8281019cd2d764f1448a0ddd733edc8b3b07d07a
atharva <atharvak3.2000@gmail.com>
2021-10-05 11:28:57 +0530

    Question2

---
+++ AssignmentNo1/Question2.js
@@ -0,0 +1,14 @@
+//Write a program print Your name 5 times on screen
+main();
+
+function Display()
+{
+     for(var i = 0;i < 5 ; i++)
+     {
+          console.log("PRAJWAL");
+     }
+}
+function main()
+{
+     Display();
+}
